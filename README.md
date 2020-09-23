133076ebfddac51f1f9dc1c5f00dd009a55fe013
Lokessler <71712901+Lokessler@users.noreply.github.com>
2020-09-23 08:00:30 -0600

    Minor program Header fixes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Assignment 7.2
 ## Peer Review Errors:
 <hr>
+<ul>
+  <li>Fix 0.1: Added sha-bang line</li>
+  <li>Fix 0.2: Replaced name and date in header with my information</li>
+  <li>Fix 0.3: Added missing parenthesis in header at (8, 93)</li>
+</ul>
--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
+
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
-# Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
+# Author: Logan Kessler 
+# Creation Date: 09-23-2020
+# Below is a simple program with 10 issues (some are syntax errors and some are logic errors). You need to identify the issues and correct them.
 
 import random
 import time
