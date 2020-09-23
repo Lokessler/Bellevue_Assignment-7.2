f132456145417e9303f3e1806ad68ddc09c04a8b
Lokessler <71712901+Lokessler@users.noreply.github.com>
2020-09-23 10:30:28 -0600

    Changed 'comment' usage

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -19,13 +19,11 @@ def displayIntro():
 
 def chooseCave():
     cave = ''
-        '''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
-
-	return caves
-        '''
+	#while cave != '1' and cave != '2':
+	#	print('Which cave will you go into? (1 or 2)')
+	#	cave = input()
+        #
+	#return caves
     while cave != '1' and cave != '2':
 	print('Which cave will you go into? (1 or 2)')
 	cave = input()
